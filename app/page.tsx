759d6d5eaac19f298e4413c5a51e1f4e644ad94a
Landon Skinner <landonjskinner@gmail.com>
2024-12-10 12:36:02 -0700

    mobile friendly centering

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Canvas from "@/app/components/drawing-canvas";
 export default function Home() {
   return (
-    <div className="p-4 h-screen w-screen font-[family-name:var(--font-nunito)] bg-cover bg-center">
+    <div className="p-4 h-[100dvh] w-screen font-[family-name:var(--font-nunito)] bg-cover bg-center">
       <main className="h-full" style={{ touchAction: "none" }}>
         <Canvas />
       </main>
